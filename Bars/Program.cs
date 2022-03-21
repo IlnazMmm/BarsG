c8fceba4085709c8d4ea6398e741d214c357cef0
Мухаметзянов Ильназ <chizescrap@mail.ru>
2022-03-21 03:07:37 +0300

    Добавьте файлы проекта.

---
+++ Bars/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Bars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task = new Task();
+            task.OnKeyPressed += (sender, symbol) => Console.WriteLine($"\nсимвол - {symbol}");
+            task.Run();
+        }
+    }
+    class Task
+    {
+        public EventHandler<char> OnKeyPressed; 
+        public void Run()
+        {
+            var symbol = Console.ReadKey();
+            OnKeyPressed?.Invoke(this, symbol.KeyChar);
+            while (symbol.KeyChar != 'c')
+            {
+                symbol = Console.ReadKey();
+                OnKeyPressed?.Invoke(this, symbol.KeyChar);
+            }
+        }
+    }
+
+}
