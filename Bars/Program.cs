0d1d169724fad8ecec7e16df25117182a89e666e
Мухаметзянов Ильназ <chizescrap@mail.ru>
2022-03-30 00:42:22 +0300

    обощения

--- Bars/Program.cs
+++ Bars/Program.cs
@@ -1,29 +1,80 @@
 ﻿using System;
+using System.IO;
 
 namespace Bars
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Task task = new Task();
-            task.OnKeyPressed += (sender, symbol) => Console.WriteLine($"\nсимвол - {symbol}");
-            task.Run();
+        static void Main()
+        {  
+            LocalFileLogger<int> t = new LocalFileLogger<int>("text.txt");
+            t.LogInfo("hello");
+            t.LogWarning("world");
+            t.LogError("ACHTUNG!!", new Exception());
+
+            LocalFileLogger<string> st = new LocalFileLogger<string>("text.txt");
+            st.LogInfo("hello");
+            st.LogWarning("ACHTUNG!!");
+            st.LogError("world", new Exception());
+
+            LocalFileLogger<Example> example = new LocalFileLogger<Example>("text.txt");
+            example.LogInfo("hello");
+            example.LogWarning("ACHTUNG!!");
+            example.LogError("world", new Exception());
         }
     }
-    class Task
+    internal interface ILogger
     {
-        public EventHandler<char> OnKeyPressed; 
-        public void Run()
+        void LogInfo(string message);
+        void LogWarning(string message);
+        void LogError(string message, Exception ex);
+    }
+    internal class LocalFileLogger<T> : ILogger
+    {
+        private string path;
+        public LocalFileLogger(string path)
         {
-            var symbol = Console.ReadKey();
-            OnKeyPressed?.Invoke(this, symbol.KeyChar);
-            while (symbol.KeyChar != 'c')
+            this.path = path;
+        }
+        public void LogInfo(string message)
+        {
+            using (StreamWriter sw = new StreamWriter(path, true))
             {
-                symbol = Console.ReadKey();
-                OnKeyPressed?.Invoke(this, symbol.KeyChar);
+                sw.WriteLine($"[Info]: [{typeof(T).Name}] : {message}");
+            };
+        }
+        public void LogWarning(string message)
+        {
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.WriteLine($"[Warning] : [{typeof(T).Name}] : {message}");
+            };
+        }
+        public void LogError(string message, Exception ex)
+        {
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.WriteLine($"[Error] : [{typeof(T).Name}] : {message}. {ex.Message}");
+            };
+        }
+    }
+    class Example
+    {
+        public string Message 
+        { 
+            get 
+            { 
+                return Message; 
+            }
+            private set
+            {
+                Message = value;
             }
         }
+        public void Print()
+        {
+            Message = "Hello World";
+            Console.WriteLine("Message");
+        }
     }
-
 }
